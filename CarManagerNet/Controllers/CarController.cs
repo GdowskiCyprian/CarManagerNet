99dcede50506dbe274474de7556ba3684bedee16
savadrox <c_gdowski@ms.wwsi.edu.pl>
2022-07-16 17:33:17 +0200

    finished CarController with required http calls

--- CarManagerNet/Controllers/CarController.cs
+++ CarManagerNet/Controllers/CarController.cs
@@ -2,6 +2,7 @@ using CarManagerNet.Helpers;
 using CarManagerNet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CarManagerNet.Controllers;
 
@@ -9,36 +10,53 @@ namespace CarManagerNet.Controllers;
 [Route("[controller]")]
 public class CarController
 {
-    //todo postCar
-    //todo deleteCar
-    //todo getCarsByClientId
-    //todo updateCar
-    
-    private DataContext _dataContext;
+    private readonly DataContext _dataContext;
     public CarController(DataContext context)
     {
         _dataContext = context;
     }
-
-    [HttpGet]
-    public async Task<List<Car>> GetCars()
-    {
-        return await _dataContext.Cars.ToListAsync();
-    }
+    //done postCar
+     [HttpPost]
+     public async void PostCar([FromBody]Car car)
+     {
+         await _dataContext.Cars.AddAsync(car);
+         await _dataContext.SaveChangesAsync();
+     }
+     //done deleteCar
+     [HttpDelete("{carId:int}")]
+     public void DeleteCarById(int carId)
+     {
+         _dataContext.Cars.RemoveRange(_dataContext.Cars.Where(x => x.CarId == carId));
+     }
+     //done getCarsByClientId
+     [HttpGet("{clientId:int}")]
+     public async Task<List<Car>> GetCarsByClientId(int clientId)
+     {
+         return await _dataContext.Cars.Where(x => x.Client.ClientId == clientId).ToListAsync();
+     }
+     //done updateCar
+     [HttpPut]
+     public void UpdateCar([FromBody]Car car)
+     {
+         _dataContext.Update(car);
+     }
     
-    [HttpGet("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Car))]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<Car> GetCarById(int id)
-    {
-        return await _dataContext.Cars.FindAsync(id);
-    }
-    [HttpPost]
-    public async void PostCar([FromBody]Car car)
-    {
-        await _dataContext.Cars.AddAsync(car);
-        await _dataContext.SaveChangesAsync();
-    }
+    //
+    // //dont think this is going to be needed later in application
+    // [HttpGet]
+    // public async Task<List<Car>> GetCars()
+    // {
+    //     return await _dataContext.Cars.ToListAsync();
+    // }
+    //
+    // [HttpGet("{id}")]
+    // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Car))]
+    // [ProducesResponseType(StatusCodes.Status404NotFound)]
+    // public async Task<Car> GetCarById(int id)
+    // {
+    //     return (await _dataContext.Cars.FindAsync(id))!;
+    // }
+
 }
 
 
